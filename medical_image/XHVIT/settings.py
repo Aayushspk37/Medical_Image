5c5fcdae7384091759f046b8c5c3d9d09d066a18
Aayush Sapkota <122710503+Aayushspk37@users.noreply.github.com>
2026-07-25 22:57:42 +0545

    Update settings.py

--- medical_image/XHVIT/settings.py
+++ medical_image/XHVIT/settings.py
@@ -15,6 +15,13 @@ from pathlib import Path
 from decouple import config
 import dj_database_url
 
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+GROQ_API_KEY = os.getenv('GROQ_API_KEY')
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
